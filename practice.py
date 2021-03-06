11536986297085e06cc1a51b7b9d827f0035e034
Nico Michel <nicomichel@utexas.edu>
2021-03-06 15:32:14 -0600

    Covered up to for loops

---
+++ practice.py
@@ -0,0 +1,34 @@
+# FUNCTION IN PYTHON 
+
+# def welcome(name='Julian', location="Meat Show"):
+#     print("Hi", name, "welcome to", location)
+
+# welcome(name="John Denver", location="My pants")
+
+
+# BOOLEANS AND CONDITIONALS
+
+# door_is_locked = True
+# if door_is_locked:
+#     print("LET ME IN")
+# else:
+#     print("I'M IN IT")
+
+# FOR LOOPS
+
+# for letters in 'Hello':
+#     print(letters)
+
+# mylist = [1, 'a', 'hey']
+# for items in mylist:
+#     print(items)
+
+# mylist = [1,2,'Hello',['a','b']]
+# print(mylist[0])
+# print(mylist[1])
+# print(mylist[0] + mylist[1])
+# print(mylist[2])
+# print(mylist[3] [0])
+
+# WHILE LOOPS
+
